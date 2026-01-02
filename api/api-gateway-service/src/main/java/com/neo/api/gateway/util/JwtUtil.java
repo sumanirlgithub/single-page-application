9fc4a10dfac369fb2fcb4d9ecd13c0a77d6457eb
suman <sumanpathak.pathak@gmail.com>
2026-01-02 17:27:41 +0000

    working with JWT token authentication at gateway level

--- api/api-gateway-service/src/main/java/com/neo/api/gateway/util/JwtUtil.java
+++ api/api-gateway-service/src/main/java/com/neo/api/gateway/util/JwtUtil.java
@@ -3,13 +3,23 @@ package com.neo.api.gateway.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtUtil {
 
-    private final String secret = "mySecretKeyForJwtValidation123456";
+    /**
+     * You need to generate JWT token from this secret and send that token through POSTMAN or client application.
+     */
+    @Value("${jwt.secret}")
+    private String secret;
 
+    /**
+     * Validate and authenticate call using JWT token supplied by client application.
+     * @param token
+     * @return Claims
+     */
     public Claims validateToken(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
